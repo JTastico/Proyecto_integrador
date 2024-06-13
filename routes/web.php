053f5f8d7775c1f0b4f8f01ac949ab9af69abfbe
Diego Apaza <diego.apaza.q@tecsup.edu.pe>
2024-06-13 13:29:46 -0500

    agregar commit en routs

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,6 @@ Route::prefix('Equipamientos')->group(function () {
 Route::prefix('Home')->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home.index');
 });
+
+
+// asd
